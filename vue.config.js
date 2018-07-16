492cb8364e8e3603edfeb9fee8e8e3761eafdc0e
Marcel Wagner <mwagner2@sapient.com>
2018-07-16 10:39:42 +0200

    basic setups added

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  pluginOptions: {
+    enableInSFC: true,
+    lintStyleOnBuild: true,
+    stylelint: {
+      fix: false,
+      syntax: 'scss',
+      files: 'src/**/*.{vue,htm,html,css,sss,less,scss}'
+    }
+  },
+
+  pwa: {
+    name: 'One Team Excercise',
+    themeColor: '#0078DC',
+    msTileColor: '#0078DC',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/myworker.js'
+      // ...other Workbox options...
+    }
+  },
+
+  lintOnSave: true,
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: true,
+  productionSourceMap: false,
+  parallel: undefined
+}
