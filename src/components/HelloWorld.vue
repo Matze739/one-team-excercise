f58dfffde5751462df94ae3760fceb2bb6b9e36c
Marcel Wagner <mwagner2@sapient.com>
2018-07-16 08:57:34 +0200

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="hello">
+    {{msg}}
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style >
+h1 {
+  color: red;
+}
+</style>
