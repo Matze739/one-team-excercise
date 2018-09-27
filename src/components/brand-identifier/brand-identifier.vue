acfb706f096e0bf897af3a15399abb5d9bf85074
alebaato <alebaato@publicisgroupe.net>
2018-09-27 11:39:03 +0200

    Added brand-identifier component, refactoring and cleanup for LH brandings

---
+++ src/components/brand-identifier/brand-identifier.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="brand-identifier">
+    <a href="http://m.staralliance.com" data-external="true"><img src="../../assets/svg/logo_alliance.svg" alt="Rebel Alliance Logo"/></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'brand-identifier'
+}
+</script>
+
+<style lang="scss">
+.brand-identifier {
+  display: flex;
+  //justify-content: right;
+  justify-content: flex-end;
+  padding: 1.125em 0;
+  border-bottom: 2px solid $ote-divider;
+  a {
+    display: flex;
+  }
+  img {
+    margin: auto 0.5em auto auto;
+  }
+}
+</style>
